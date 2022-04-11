3f035a5614677245b471f9527d765a6725d9e16f
ImScarecrow5 <94841246+ImScarecrow5@users.noreply.github.com>
2022-04-11 11:42:58 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 button_hi = KeyboardButton('Привет! 👋')
@@ -11,21 +10,11 @@ button_back = KeyboardButton('Назад')
 button_info = KeyboardButton('Информация о командах')
 
 button_ras = KeyboardButton('Рассылка')
+button_rasphoto = KeyboardButton('Рассылка фото')
+
 
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False).add(button_hi)
 greet_kb1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False).add(button_ym).add(button_info).add(button_hi)
 markup3 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_ym).add(button_keyboardss).add(button_hi)
 admin_panel = ReplyKeyboardMarkup(resize_keyboard=True).add(button_back).add(button_ras).add(button_info)
-
-
-
-
-
-inline_btn_1 = InlineKeyboardButton('Что ты умеешь? 🤔', callback_data='button1')
-
-inline_btn_2 = InlineKeyboardButton('Рассылка(/sendall)', callback_data='button2')
-
-inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
-inline_kb2 = InlineKeyboardMarkup().add(inline_btn_2)
-
 
