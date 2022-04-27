e46921476dff0e959232f5acaf46a13be1bb1644
ImScarecrow5 <94841246+ImScarecrow5@users.noreply.github.com>
2022-04-27 23:15:19 +0300

    most likely the latest version

--- keyboards.py
+++ keyboards.py
@@ -6,14 +6,9 @@ button_ym = KeyboardButton('Что ты умеешь? 🤔')
 button_keyboardss = KeyboardButton('Клавиатура (только для админов)')
 button_back = KeyboardButton('Назад')
 
-
 button_info = KeyboardButton('Информация о командах')
 
-button_ras = KeyboardButton('Рассылка')
-button_rasphoto = KeyboardButton('Рассылка фото')
 
-
-greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False).add(button_hi)
+greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button_hi)
 greet_kb1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False).add(button_ym).add(button_info).add(button_hi)
-markup3 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_ym).add(button_keyboardss).add(button_hi)
-admin_panel = ReplyKeyboardMarkup(resize_keyboard=True).add(button_ras).add(button_rasphoto).add(button_info).add(button_back)
+markup3 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_ym).add(button_info).add(button_hi)
